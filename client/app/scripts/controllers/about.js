aa34285348da964dc617821af34833bb5f38cb8e
tqi <tom.qi@twc-Temp.com>
2016-04-14 14:07:43 -0700

    yomen angular

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dingdangApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dingdangApp
+ */
+angular.module('dingdangApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dingdangApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dingdangApp
+ */
+angular.module('dingdangApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
