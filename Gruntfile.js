74ffaffba5f0ac65b53f9b2875386209dd7c0f95
qixiaohuihaha <qixiaohuihaha@gmail.com>
2016-04-22 13:33:09 -0700

    add grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'nodeunit']
+      }
+    },
+    replace: {
+      dev: {
+        overwrite: true,
+        src: ['./util/url.js', './client/app/scripts/services/service.js'],
+        replacements: [{
+          from: 'mongodb://heroku_qdqpxftz:fjn6sspav936frbv4tml0hpmcj@ds013971.mlab.com:13971/heroku_qdqpxftz',                   // string replacement 
+          to: 'mongodb://localhost:27017/USNEWS'
+        },{
+          from: 'http://polar-sands-49796.herokuapp.com/news/',                   
+          to: 'http://localhost:2000/news/'          
+        }]
+      },
+      deploy: {
+        overwrite: true,
+        src: ['./util/url.js', './client/app/scripts/services/service.js'],
+        replacements: [{
+          to: 'mongodb://heroku_qdqpxftz:fjn6sspav936frbv4tml0hpmcj@ds013971.mlab.com:13971/heroku_qdqpxftz',                   // string replacement 
+          from: 'mongodb://localhost:27017/USNEWS'
+        },{
+          to: 'http://polar-sands-49796.herokuapp.com/news/',                    
+          from: 'http://localhost:2000/news/'          
+        }]
+      },
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-text-replace');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+  //Switch to dev
+  grunt.registerTask('dev', ['replace:dev']);
+
+  //switch to deploy
+  grunt.registerTask('deploy', ['replace:deploy']);
+
+};
--- util/url.js
+++ util/url.js
@@ -8,5 +8,5 @@ exports.URL = {
 			cloud: "https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&start=now-1d&end=now&count=50&q.enriched.url.enrichedTitle.taxonomy.taxonomy_.label=technology+and+computing&q.enriched.url.title=cloud&return=enriched.url.title,enriched.url.image,enriched.url.url,enriched.url.publicationDate.date,enriched.url.text&apikey=57dd8e61e1837f3c3817d9675ab56ad6785f2183"
 		}
 	},
-	mongo_base: "mongodb://heroku_qdqpxftz:fjn6sspav936frbv4tml0hpmcj@ds013971.mlab.com:13971/heroku_qdqpxftz"
+	mongo_base: "mongodb://localhost:27017/USNEWS"
 };
