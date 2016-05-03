ed90fcbb6dd6b96c009e8fbf00b169afe18e9cfd
qixiaohuihaha <qixiaohuihaha@gmail.com>
2016-05-02 20:20:44 -0700

    remove webworker

--- dao/news_scraper.js
+++ dao/news_scraper.js
@@ -5,37 +5,23 @@ var exports = module.exports = {};
 var Xray = require('x-ray');
 var x = Xray();
 var read = require('node-readability');
-var worker = require('webworker-threads').Worker;
 
 exports.scrape = function(req, res, link, id){
-	var scraper = new worker(function(){
-		this.onmessage = function(event){
-			postMessage(
-				read(link, {}, function(err, article) {
-					if(err){
-						res.send({});
-					}else{
-						if(article.content){
-							mongoApi.insertContent(article.content, id);
-							article.close();
-							return [{content: article.content}];
-						}else{
-							mongoApi.insertContent(article.html, id);
-							article.close();
-							return [{content: article.html}];
-						}
-					}
+	read(link, {}, function(err, article) {
+		if(err){
+			res.send({});
+		}else{
+			if(article.content){
+				mongoApi.insertContent(article.content, id);
+				res.send([{content: article.content}]);
+			}else{
+				mongoApi.insertContent(article.html, id);
+				res.send(article.html);
+			}
+			article.close();
+		}
 
-				})
-			);
-		};
 	});
-
-	scraper.onmessage = function(event){
-		res.send(event.data);
-	};
-
-	scraper.post();
 
 	// x(link, 'p', [{paragraph: ''}])(function(err, content) {
 	// 	console.log(content);
